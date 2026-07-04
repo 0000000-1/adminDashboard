3c20707c6176cdb81d06373e7a4e8518faaa1e0f
sumit <rajaji9968@gmail.com>
2026-07-05 00:34:07 +0530

    file commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,18 @@ import dotenv, { config } from 'dotenv'
 dotenv.config()
 
 const mongo_url = process.env.MONGO_URI
-const database = new mongoose.connect("",()=>{
 
-})
+// const dbs = async (()=>{
+
+// })
+const database = async ((req, res) => {
+   try {
+      const db = new database.connect(mongo_url)
+      console.log('wokin mng')
+   } catch (error) {
+      res.status(500).json({ message: 'not connected to database' })
+   }
+}
+)
+// export dbs = module.database
+export default database
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from "dotenv"
+import database from './config/db.js'
 
 const app = express()
 dotenv.config()
 
+database()
 app.get('/',(req,res)=>{
     res.send('server working sucessfully!')
     console.log("working")
